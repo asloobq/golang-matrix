e7b1a2cff6ec9e2379b929b80b7682067e6cc9cd
Asloob Qureshi <asloobq@gmail.com>
2014-03-20 21:05:05 -0400

    Misc changes

--- MatAddStatic.go
+++ MatAddStatic.go
@@ -5,9 +5,9 @@ import (
         "math/rand"
        )
 
-//Allcoates Memory and returns matrix
+//Allocates Memory and returns matrix
 
-func allocate_mat(size int, ch chan [][]int) {
+func allocateMat(size int, ch chan [][]int) {
 	// Allocate Memory
 mat := make([][]int, size)
 	     for i := range mat {
@@ -17,7 +17,7 @@ mat := make([][]int, size)
 }
 
 
-func initialize_mat(mat [][]int, size int, ch chan [][]int){
+func initializeMat(mat [][]int, size int, ch chan [][]int){
     //var seed int64
     //seed = 10
     for i := 0; i < size; i++ {
@@ -33,7 +33,7 @@ func initialize_mat(mat [][]int, size int, ch chan [][]int){
 func print(mat [][]int, size int) {
     for i := 0; i < size; i++ {
         for j := 0; j < size; j++ {
-            fmt.Printf("ary[%d][%d] = %d", i, j, mat[i][j]);
+            fmt.Printf("ary[%d][%d] = %d  ", i, j, mat[i][j]);
         }
         fmt.Println("");
     }
@@ -43,33 +43,47 @@ func print(mat [][]int, size int) {
 func main() {
 	var size int
 		
-		chA := make(chan [][]int)					//Creating channel for matrix A
-		//	var b [][]int
-		// 	var c [][]int
+		chA := make(chan [][]int)			//Creating channel for matrix A
+		chB := make(chan [][]int)			//Creating channel for matrix B
+		chC := make(chan [][]int)			//Creating channel for matrix C
 
-		//ch := make(chan bool)					//Channel to wait for allocation
+        //Accept matrix size as input
 		fmt.Print("enter size: ")
 		fmt.Scan(&size)
 
 
 		//Allcoating Memory of Matrices concurrently
-		go allocate_mat( size, chA)
-		//go allocate_mat(b, size, ch)
-		//go allocate_mat(c, size, ch)
+		go allocateMat(size, chA)
+		go allocateMat(size, chB)
+		go allocateMat(size, chC)
 
-		//for i := 0 ; i < 3 ; i++{
-		a := <-chA
-		//}
+		//Wait for allocation
+		matA := <-chA
+		matB := <-chB
+        matC := <-chC
 
         // Initialize matrix A
-        go initialize_mat(a, size, chA)
-        a = <- chA
+        go initializeMat(matA, size, chA)
+        go initializeMat(matB, size, chB)
+        go initializeMat(matC, size, chC)
 
+        //Wait for initialization
+        matA = <-chA
+    	matB = <-chB
+        matC = <-chC
+    
         //Print matrix A
-        print(a, size);
-
-		// array elements initialized to 0
-		//fmt.Println("a[0][0] =", a[0][0])
+        fmt.Println("Matrix A loaded with random numbers:")
+        print(matA, size)
+        
+        fmt.Println("Matrix B loaded with random numbers:")
+        print(matB, size)
+        
+        fmt.Println("Matrix C loaded with random numbers:")
+		print(matC, size)
+		
+        
+        //fmt.Println("a[0][0] =", a[0][0])
 
 		// assign
 		//a[size-1][size-1] = 7
@@ -78,6 +92,6 @@ func main() {
 		//fmt.Printf("a[%d][%d] = %d\n", size-1, size-1, a[size-1][size-1])
 
 		// remove only reference
-		a = nil
+		//a = nil
 		// memory allocated earlier with make can now be garbage collected.
 }
