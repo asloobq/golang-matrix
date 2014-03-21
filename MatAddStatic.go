69ad3b98ef1d125707a741fbfca264fa157d9965
Asloob Qureshi <asloobq@gmail.com>
2014-03-20 22:46:08 -0400

    Accepting splits and size from terminal. Merged changes.

--- MatAddStatic.go
+++ MatAddStatic.go
@@ -13,16 +13,16 @@ import (
    Calculates addition of the matrices A and B for row 
    numbers [start, end) 
  */
-func addMatBlock(matA [][]int, matB [][]int, matC [][]int, 
-		size int, start int, end int, ch chan bool){
+func addMatBlock(matA [][]int, matB [][]int, matC [][]int, cols int, 
+        ch chan bool){
+rows := len(matA)
+          for i := 0; i < rows; i++ {
+              for j := 0; j < cols; j++ {
+                  matC[i][j] = matA[i][j] + matB[i][j]
+              }
+          }
 
-	for i := start; i < end; i++ {
-		for j := 0; j < size; j++ {
-			matC[i][j] = matA[i][j] + matB[i][j]
-		}
-	}
-
-	ch <- true
+      ch <- true
 }
 
 /*
@@ -30,39 +30,59 @@ func addMatBlock(matA [][]int, matB [][]int, matC [][]int,
    Calls 'addMatBlock' split number of times.
 
  */
-func addMatStatic(matA [][]int, matB [][]int, matC [][]int, 
-		size int, splits int, ch chan bool){
+func addMatStatic(matA [][]int, matB [][]int, matC [][]int, splits int,
+        ch chan bool){
 
-	//chBool := make(chan bool)
-	//chunkSize := size / splits
-	for i:= 0; i < splits; i++ {
-		//addMatBlock(matA, matB, matC, size, i*chunkSize, 
-		//        i*chunkSize + chunkSize, chBool)
+size := len(matA)   //number of rows and columns
+          chBool := make(chan bool)
+          chunkSize := size / splits  //rows to compute by 1 go routine
 
-	}
+          fmt.Printf("addMatStatic chunkSize = %d \n", chunkSize)
 
-	ch <- true
+          i := 0
+          for ; i <= (splits-2); i++ {
+
+start := i*chunkSize
+           end := start + chunkSize
+           go addMatBlock(matA[start:end], matB[start:end], matC[start:end], size,
+                   chBool)
+           fmt.Printf("matAddStatic i = %d \n", i)
+          }
+
+      //handle last split separately
+start := i*chunkSize
+           go addMatBlock(matA[start:], matB[start:], matC[start:], size,
+                   chBool)
+           fmt.Printf("matAddStatic i = %d \n", i)
+
+           for i:= 0; i < splits; i++ {
+               <- chBool
+                   fmt.Printf("matAddStatic sync i = %d\n", i)
+           }
+
+       ch <- true;
+
 }
 
 func allocateMat(size int, ch chan [][]int) {
-	// Allocate Memory
+    // Allocate Memory
 mat := make([][]int, size)
-	     for i := range mat {
-		     mat[i] = make([]int, size)
-	     }
+         for i := range mat {
+             mat[i] = make([]int, size)
+         }
      ch <- mat
 }
 
 
-func initializeMat(mat [][]int, size int, ch chan bool){
+func initializeMat(mat [][]int, ch chan bool){
+size := len(mat)
+          for i := 0; i < size; i++ {
+              for j := 0; j < size; j++ {
+                  mat[i][j] = rand.Intn(10);
+              }
+          }
 
-	for i := 0; i < size; i++ {
-		for j := 0; j < size; j++ {
-			mat[i][j] = rand.Intn(10);
-		}
-	}
-
-	ch <- true
+      ch <- true
 }
 
 func print(mat [][]int, size int) {
@@ -87,49 +107,50 @@ func main() {
 			
 			size, _ := strconv.Atoi(os.Args[2])
 			//Allcoating Memory of Matrices concurrently
-				go allocateMat(size, chA)
-				go allocateMat(size, chB)
-				go allocateMat(size, chC)
+        go allocateMat(size, chA)
+        go allocateMat(size, chB)
+        go allocateMat(size, chC)
 
-				//Wait for allocation
-				matA := <-chA
-				matB := <-chB
-				matC := <-chC
+        //Wait for allocation
+        matA := <-chA
+        matB := <-chB
+        matC := <-chC
 
 
-				chABool := make(chan bool) 
-				chBBool := make(chan bool)
-				// chCBool := make(chan bool)
+        chBool := make(chan bool) 
+        // chBBool := make(chan bool)
+        // chCBool := make(chan bool)
 
-				// Initialize matrix A
-				go initializeMat(matA, size, chABool)
-				go initializeMat(matB, size, chBBool)
-				//go initializeMat(matC, size, chCBool)
+        // Initialize matrix A
+        go initializeMat(matA, chBool)
+        go initializeMat(matB, chBool)
+        //go initializeMat(matC, chCBool)
 
-				//Wait for initialization
-				<- chABool
-				<- chBBool
-				//<- chCBool
+        //Wait for initialization
+        <- chBool
+        <- chBool
+        //<- chCBool
 
-				//Print matrix A
-				fmt.Println("Matrix A loaded with random numbers:")
-				print(matA, size)
+        //Print matrix A
+        fmt.Println("Matrix A loaded with random numbers:")
+        print(matA, size)
 
-				fmt.Println("Matrix B loaded with random numbers:")
-				print(matB, size)
+        fmt.Println("Matrix B loaded with random numbers:")
+        print(matB, size)
 
-				fmt.Println("Matrix C loaded with -1s:")
-				print(matC, size)
-				/*
-				//Do addition
-				go addMatBlock(matA, matB, matC, size, 0, size, chC)
-				matC = <- chC        
+        fmt.Println("Matrix C loaded with -1s:")
+        print(matC, size)
 
-				fmt.Println("Matrix C holds result after A[] + B[]:")
-				print(matC, size)
-				 */
-		} else{
-			fmt.Println("usage ./<executable> <splits> <size>");
-		}	
+        /********  ADDITION  ***********/
 
+        splits := 2
+        //Do addition
+        go addMatStatic(matA, matB, matC, splits, chBool)
+        <- chBool
+
+        fmt.Println("Matrix C holds result after A[] + B[]:")
+        print(matC, size)
+        } else {
+            fmt.Println("usage is ./executable <splits> <size>")
+        }
 }
